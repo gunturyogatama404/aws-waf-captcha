12cd2f2874b7131f80bac75f3126d94238964651
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2025-11-01 18:33:17 +0700

    Update _ai.py

--- lib/_ai.py
+++ lib/_ai.py
@@ -1,39 +1,43 @@
 import os
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import numpy as np
-import keras
 import base64
-
+import numpy as np
+import threading
 from io import BytesIO
 from PIL import Image
+from keras.models import load_model
 
-model: keras.Model = keras.models.load_model('./models/1.keras')
-
+_model = None
+_model_lock = threading.Lock()
 ANSWERS = ['bag', 'bed', 'bucket', 'chair', 'clock', 'curtain', 'hat']
 
-def get_predictions(images):
-    """ Get predictions of image classification """
-    _model: keras.Model = keras.models.clone_model(model)
-    _model.set_weights(model.get_weights())
-    predictions = []
-    for image in images:
-        image_io = BytesIO(base64.b64decode(image))
-        image = np.asarray(Image.open(image_io)).reshape(1, 100, 100, 3)
-        
-        prediction = _model(image).numpy() # <- thread safe
-        predictions.append(prediction)
+def get_model():
+    global _model
+    with _model_lock:
+        if _model is None:
+            print("[INFO] Loading CAPTCHA model...")
+            _model = load_model('./models/1.keras')
+            print("[INFO] Model loaded successfully!")
+    return _model
 
-    return predictions
+def preprocess_image(image_b64: str) -> np.ndarray:
+    image_io = BytesIO(base64.b64decode(image_b64))
+    img = Image.open(image_io).convert("RGB").resize((100, 100))
+    arr = np.asarray(img, dtype=np.float32) / 255.0
+    return arr
 
 def get_solutions(images: list[str], target: str):
-    """ Solve an AWS captcha """
-    solutions = []
-    predictions = get_predictions(images)
-    for i, prediction in enumerate(predictions, 0):
-        answer = ANSWERS[prediction.argmax(1)[0]]
-        if answer == target:
-            solutions.append(i)
+    model = get_model()
+    imgs = [preprocess_image(img) for img in images]
+    batch = np.stack(imgs, axis=0)
+
+    with _model_lock:
+        preds = model(batch).numpy()
+
+    solutions = [
+        i for i, pred in enumerate(preds)
+        if ANSWERS[np.argmax(pred)].lower() == target.lower()
+    ]
 
     return solutions
